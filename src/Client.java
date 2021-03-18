ca7861e641cb7f30727613398adce04757419770
Canh Thanh Nguyen <ncthanh2000@yahoo.com>
2021-03-18 22:55:16 +1100

    Change readXML to return Servers
    Change ParseFloat to ParseDouble.
    readXML now return an arrayList of serverObjects

--- src/Client.java
+++ src/Client.java
@@ -10,12 +10,10 @@ import org.w3c.dom.NodeList;
 
 public class Client {
 
-
-
-    public static void readXML(){
-        try {
+    public static ArrayList<ServerObject> readXML(){
+		ArrayList<ServerObject> serversList = new ArrayList<ServerObject>();
+		try {
 			File systemXML = new File("./pre-compiled/ds-system.xml");
-
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(systemXML);
@@ -27,15 +25,19 @@ public class Client {
 				String t = server.getAttribute("type");
 				int l = Integer.parseInt(server.getAttribute("limit"));
 				int b = Integer.parseInt(server.getAttribute("bootupTime"));
-				float hr = Float.parseFloat(server.getAttribute("hourlyRate"));
+				double hr = Double.parseDouble(server.getAttribute("hourlyRate"));
 				int c = Integer.parseInt(server.getAttribute("coreCount"));
 				int m = Integer.parseInt(server.getAttribute("memory"));
 				int d = Integer.parseInt(server.getAttribute("disk"));
+
+				ServerObject serv = new ServerObject(t,l,b,hr,c,m,d);
+				serversList.add(serv);
 				System.out.print(t+"\n");
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+        return serversList;
     }
 	public static void sendToServer(){
 
