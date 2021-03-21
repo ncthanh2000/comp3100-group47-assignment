4943c5a68b299954500b018e6f4533b7aaab751f
SocheatChhun <69142431+SocheatChhun@users.noreply.github.com>
2021-03-21 17:16:29 +1100

    put send message to server in a method

--- src/Client.java
+++ src/Client.java
@@ -10,10 +10,31 @@ import org.w3c.dom.NodeList;
 
 public class Client {
 
-    public static ArrayList<ServerObject> readXML(){
+	public Socket s = null;
+	public BufferedReader input = null;
+	public DataOutputStream output = null;
+
+	public Client(String localAddress, int port) {
+
+		try {
+			s = new Socket("localhost", 50000);
+			input = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			output = new DataOutputStream(s.getOutputStream());
+		} catch (UnknownHostException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+
+	}
+
+	// function to read xml file server from the ds-server
+	public ArrayList<ServerObject> readXML() {
 		ArrayList<ServerObject> serversList = new ArrayList<ServerObject>();
 		try {
-			File systemXML = new File("./pre-compiled/ds-system.xml");
+			File systemXML = new File("./ds-system.xml");
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(systemXML);
@@ -22,83 +43,89 @@ public class Client {
 			NodeList servers = doc.getElementsByTagName("server");
 			for (int i = 0; i < servers.getLength(); i++) {
 				Element server = (Element) servers.item(i);
-				String t = server.getAttribute("type");
-				int l = Integer.parseInt(server.getAttribute("limit"));
-				int b = Integer.parseInt(server.getAttribute("bootupTime"));
-				double hr = Double.parseDouble(server.getAttribute("hourlyRate"));
-				int c = Integer.parseInt(server.getAttribute("coreCount"));
-				int m = Integer.parseInt(server.getAttribute("memory"));
-				int d = Integer.parseInt(server.getAttribute("disk"));
+				String type = server.getAttribute("type");
+				int limit = Integer.parseInt(server.getAttribute("limit"));
+				int bootUpTime = Integer.parseInt(server.getAttribute("bootupTime"));
+				double hourlyRate = Double.parseDouble(server.getAttribute("hourlyRate"));
+				int coreCount = Integer.parseInt(server.getAttribute("coreCount"));
+				int memory = Integer.parseInt(server.getAttribute("memory"));
+				int disk = Integer.parseInt(server.getAttribute("disk"));
 
-				ServerObject serv = new ServerObject(t,l,b,hr,c,m,d);
+				ServerObject serv = new ServerObject(type, limit, bootUpTime, hourlyRate, coreCount, memory, disk);
+				// Add all this server we read from xml files to a server class that we created
 				serversList.add(serv);
-				System.out.print(t+"\n");
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-        return serversList;
-    }
-	public static void sendToServer(){
+		return serversList;
+	}
+
+	// method to send to the server
+	public void sendToServer(String message) {
+		try {
+			output.write(message.getBytes());
+			output.flush();
+
+		} catch (IOException e) {
+			System.out.println(e);
+		}
+	}
+	
+	//main method to do all the client handling
+	public void start() {
+		try {
+			String username = System.getProperty("user.name");
+			String authMessage = "AUTH " + username;
+
+			sendToServer("HELO");
+			sendToServer(authMessage);
+			sendToServer("REDY");
+
+			ArrayList<ServerObject> serverList=readXML();
+
+			System.out.println(serverList.toString());
+			
+			// sendToServer("REDY");
+
+			s.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public static boolean checkArgs(String[] argument)
-	{
+	public static boolean checkArgs(String[] argument) {
 		boolean flag = false;
 		if (argument == null || argument.length != 2) {
 			System.out.print("Invalid argument length");
 		}
 
-		else if (!argument[0].equals("-a"))
-		{
+		else if (!argument[0].equals("-a")) {
 			System.out.print("Missing argument: -a");
 		}
 
-
 		else if (!argument[1].equals("predefinedAlgo"))
-		// argument[1] has to be in the list of predefinedAlgo. We do something like predefinedAlgo.contains(argument[1])
+		// argument[1] has to be in the list of predefinedAlgo. We do something like
+		// predefinedAlgo.contains(argument[1])
 		{
 			System.out.print("For argument: -a, Invalid choice: " + (argument[1]));
 		}
 
-		else
-		{
+		else {
 			System.out.print("Successful function call");
 			flag = true;
 		}
 		return flag;
 	}
 
+	public static void main(String[] args) {
+		Client client = new Client("127.0.0.1", 50000);
+		// boolean validArg = checkArgs(args);
+		// DO SOME ARGUMENT CHECKING
+		client.start();
+		System.out.println("Hello world");
 
-    public static void main(String[] args) {
-		try {
-
-			// DO SOME ARGUMENT CHECKING
-			boolean validArg = checkArgs(args);
-
-
-            String username=System.getProperty("user.name");
-            String message = "AUTH " + username;
-            Socket s = new Socket("localhost", 50000);
-
-	        BufferedReader input = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            DataOutputStream output = new DataOutputStream(s.getOutputStream());
-			
-            output.write("HELO".getBytes());
-            output.write(message.getBytes());
-			readXML();
-
-            output.write("REDY".getBytes());
-
-            output.flush();
-            output.close();
-            s.close();
-			System.out.println("Hello world");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-
-    }
+	}
 }
