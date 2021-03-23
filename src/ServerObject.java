353e8530147c3a2a38e2cc56338781b76f515739
mdhasan007 <hasanmdmehedy007@gmail.com>
2021-03-24 10:26:32 +1100

    create compareTo to get largest server, added instanceCopy constructor

--- src/ServerObject.java
+++ src/ServerObject.java
@@ -20,4 +20,38 @@ public class ServerObject {
     public String toString(){
         return type+", "+limit+", "+bootupTime+", "+hourlyRate+", "+coreCount+", "+memory+", "+disk;
     }
+	
+	public ServerObject(ServerObject other) {
+        this.type = other.type;
+        this.limit = other.limit;
+        this.bootupTime = other.bootupTime;
+        this.hourlyRate = other.hourlyRate;
+        this.coreCount = other.coreCount;
+        this.memory = other.memory;
+        this.disk = other.disk;
+    }
+
+	public int compareTo(ServerObject other)
+    {
+        // Comparison ordering: type -> coreCount -> memory -> disk
+        List<String> typeCompare = Arrays.asList("tiny", "small","medium","large", "xlarge");
+        // using typeCompare.indexOf to compare since we know that there's a
+        // fixed number of "types"
+        int typeComp  = 0;
+        if(typeCompare.indexOf(this.type) < typeCompare.indexOf(other.type))
+        {
+            typeComp = -1;
+        }
+
+        if(typeCompare.indexOf(this.type) > typeCompare.indexOf(other.type))
+        {
+            typeComp = 1;
+        }
+
+        int coreComp = Integer.compare(this.coreCount, other.coreCount);
+        int memoryComp = Integer.compare(this.memory, other.memory);
+        int diskComp = Integer.compare(this.disk, other.disk);
+
+        return typeComp!=0?typeComp: coreComp!=0?coreComp: memoryComp!=0?memoryComp: diskComp;
+    }
 }
